811860e0a265a761b8bbe53c8ce1cf95a3874204
Lutonin Daniil <lutonindm@gmail.com>
2025-07-14 17:39:33 +0600

    feat: plugins system

---
+++ PluginSystem/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+namespace PluginSystem;
+
+public class PluginLoadAttribute : Attribute
+{
+    public string[] Dependencies { get; }
+    public PluginLoadAttribute(params string[] dependencies) => Dependencies = dependencies;
+}
+
+public class PluginSystem
+{
+    public static void Run()
+    {
+        var pluginFolder = Path.Combine("Plugins");
+        var plugins = Directory.EnumerateFiles(pluginFolder, "*.dll")
+            .Select(Assembly.LoadFrom)
+            .SelectMany(a => a.GetExportedTypes())
+            .Where(t => t.GetCustomAttribute<PluginLoadAttribute>() != null)
+            .ToList();
+        
+        plugins.ForEach(t => 
+        {
+            var instance = Activator.CreateInstance(t);
+            t.GetMethod("Execute")?.Invoke(instance, null);
+        });
+    }
+}
+
+public class App
+{
+    public static void Main(string[] args) => PluginSystem.Run();
+}
---
+++ TestPlugin/Class1.cs
@@ -0,0 +1,8 @@
+﻿using PluginSystem;
+namespace TestPlugin;
+
+[PluginLoad]
+public class TestPlugin
+{
+    public void Execute() => Console.WriteLine("Plugin 'TestPlugin' is used");
+}
